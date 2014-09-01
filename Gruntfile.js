fe9a668a292767f130f22f65dbdf9d416bbc8cc4
phette23 <phette23@gmail.com>
2014-09-01 15:56:45 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,157 @@
+module.exports = function (grunt) {
+    'use strict';
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+    var configFile = grunt.option('config') || 'config.json',
+        config = grunt.file.readJSON(configFile);
+
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                'app/js/app.js'
+            ]
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'none'
+                  },
+                  files: {
+                    'build/css/main.min.css': 'build/css/main.scss'
+                  }
+            }
+        },
+        scsslint: {
+            dist: [ 'app/css/*.scss' ],
+            options: {
+                force: true
+            }
+        },
+        uglify: {
+            dist: {
+                options: {
+                  compress: {
+                    drop_console: true
+                  },
+                  wrap: true
+                },
+                files: {
+                    'build/js/app.min.js': ['build/js/app.js']
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeCommentsFromCDATA: true,
+                    // https://github.com/yeoman/grunt-usemin/issues/44
+                    collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    removeAttributeQuotes: true,
+                    removeRedundantAttributes: true,
+                    useShortDoctype: true,
+                    removeEmptyAttributes: true,
+                    removeOptionalTags: true,
+                    removeComments: true
+                },
+                files: {
+                    'build/index.html': 'build/index.html'
+                }
+            }
+        },
+        // copy just moves images into build/
+        // CSS & scripts are inlined in HTML before this step
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'app',
+                    dest: 'build',
+                    src: ['img/*.{png,jpg,jpeg,gif,ico}']
+                }]
+            }
+        },
+        'string-replace': {
+            // swaps out template strings (e.g. {{prefix}})
+            // with values in config.json
+            // moves CSS, JS, HTML => build/
+            tpl: {
+                files: {
+                    'build/js/app.js': 'app/js/app.js',
+                    'build/css/main.scss': 'app/css/main.scss',
+                    'build/css/_vars.scss': 'app/css/_vars.scss',
+                    'build/index.html': 'app/index.html'
+                },
+                options: {
+                    replacements: [{
+                        pattern: /\{\{prefix\}\}/i,
+                        replacement: config.prefix
+                    }, {
+                        pattern: /\{\{largeImageUrl\}\}/i,
+                        replacement: config.largeImageUrl
+                    }, {
+                        pattern: /\{\{smallImageUrl\}\}/i,
+                        replacement: config.smallImageUrl
+                    }, {
+                        pattern: /\{\{link\[(\d)\]\.([a-z0-9]*)\}\}/ig,
+                        replacement: function (match, p1, p2) {
+                            if (config.links[p1]) {
+                                return config.links[p1][p2];
+                            }
+                        }
+                    }]
+                }
+            },
+            // inline app.js & main.css
+            // runs AFTER tpl, SASS, & Uglify tasks
+            inline: {
+                files: {
+                    'build/index.html': 'build/index.html'
+                },
+                options: {
+                    replacements: [{
+                        pattern: '<script src="js/app.js"></script>',
+                        replacement: '<script><%= grunt.file.read("build/js/app.min.js") %></script>'
+                    }, {
+                        pattern: '<style>@import url(css/main.css);</style>',
+                        replacement: '<style><%= grunt.file.read("build/css/main.min.css") %></style>'
+                    }]
+                }
+            }
+        },
+        clean: {
+            dist: {
+                src: ['build']
+            }
+        },
+        watch: {
+            dist: {
+                files: ['app/**'],
+                tasks: ['build']
+            }
+        }
+    });
+
+    grunt.registerTask('lint', [
+        'jshint',
+        'scsslint'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'string-replace:tpl',
+        'uglify',
+        'sass',
+        'string-replace:inline',
+        'htmlmin'
+    ]);
+
+    // @todo serve task
+};
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Portable Header
+
+HTML header meant for inclusion in LibGuides, OPACs, & other third-party apps where you can paste some HTML in a textarea but not much else.
+
+# Templating
+
+The intention is to make this header as reusable as possible simply by specifying different configurations. To build a new header, run `grunt build` (see **Getting Started** section below) & then copy the output inside "build/index.html". To create a new configuration, write your own config.json & then point to it with the "config" flag. So for a "libguides.json" I created, I might run
+
+```bash
+grunt build --config=libguides.json
+```
+
+Inside config.json, you define text that will be replaced in the header's code. Currently, you can specify an institutional prefix to avoid code conflicts (e.g. a site might already have an `"id=header"` element), five links and their link text, and two logo images (one for larger screens and one for small). The easiest way to customize is to edit config.json but keep the existing text the same. The configuration must be valid JSON.
+
+There are 2 example configurations in the project, including one for the California College of the Arts in "cca.config.json".
+
+# Getting Started
+
+To work on this using its development tools:
+
+- Install [NodeJS](http://nodejs.org/)
+- Install [Grunt](http://gruntjs.com/) globally; on the command line, run `npm install -g grunt-cli`
+- Inside this project, install its dependencies: `npm install`
+
+There are 3 major Grunt tasks available:
+
+- `grunt build` compiles the SASS into CSS, inlines the CSS & JavaScript into index.html, & puts the final product in a new "build" folder
+- `grunt watch` watches for any changes to files inside the "app" folder & then runs `grunt build`
+- `grunt lint` tests your code quality (JSHint on JavaScript, SCSS-lint for SASS)
+
+# To Do
+
+- [x] Image URLs as part of config.json
+- [x] Explain JSON options
+- [ ] Example on GitHub pages
+- [ ] Bigger touch targets (`<a>` inside `<li>` should be larger)
+- [ ] Variable number of links?
